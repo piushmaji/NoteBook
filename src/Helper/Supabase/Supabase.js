ccb3a60d5549abd7773be9d6e2ab857dc8678ea3
Piush Maji <piushmaji@gmail.com>
2026-03-03 22:12:42 +0530

    add .env
    Add variable keys and variable annon key on netlify and it works perfectly.Add Environmental Variables

--- src/Helper/Supabase/Supabase.js
+++ src/Helper/Supabase/Supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = 'https://xgvqxeuzpwckxmxyihsw.supabase.co'
-const supabaseAnnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhndnF4ZXV6cHdja3hteHlpaHN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTA4OTAsImV4cCI6MjA4ODAyNjg5MH0.rORY3PThvazlKU8Seu_N18VY5a3S5z8G641kZbczBdk"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnnonKey)
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
